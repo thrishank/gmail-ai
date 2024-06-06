83cf0cfd4cff7de7dfeaee0233579473e85162a1
thrishank <thrishankkalluru@gmail.com>
2024-06-06 15:47:09 +0530

    added the gmail api

--- app/api/gmail/route.ts
+++ app/api/gmail/route.ts
@@ -1,6 +1,33 @@
 import { authoptions } from '@/lib/auth';
+import { google } from 'googleapis';
 import { getServerSession } from 'next-auth';
 import { getSession } from 'next-auth/react';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {}
+export async function GET(req: NextRequest, res: NextResponse) {
+  const session = await getServerSession(authoptions);
+  if (!session) return NextResponse.json('Not authorized');
+
+  const auth = new google.auth.OAuth2();
+  auth.setCredentials({ access_token: session.user.accessToken });
+
+  const gmail = google.gmail({ version: 'v1', auth });
+
+  //   return NextResponse.json(session)
+  try {
+    const response = await gmail.users.messages.list({
+      userId: 'me',
+      maxResults: 5,
+    });
+    const messageIds = response.data.messages?.map((item) => item.id) || [];
+    const messages = await Promise.all(
+      messageIds.map(async (id) => {
+        const msg = await gmail.users.messages.get({ userId: 'me', id: id! });
+        return msg.data;
+      }),
+    );
+    return NextResponse.json(messages);
+  } catch (Err) {
+    return NextResponse.json(Err);
+  }
+}
--- lib/auth.ts
+++ lib/auth.ts
@@ -27,6 +27,20 @@ export const authoptions = {
     }),
   ],
   callbacks: {
+    async session({ session, token }: any) {
+      session.user = {
+        name: token.name,
+        email: token.email,
+        picture: token.picture,
+        sub: token.sub,
+        accessToken: token.accessToken,
+        iat: token.iat,
+        exp: token.exp,
+        jti: token.jti,
+        id: token.id,
+      };
+      return session;
+    },
     async jwt({ token, account, profile }: any) {
       if (account) {
         token.accessToken = account.access_token;
@@ -35,27 +49,8 @@ export const authoptions = {
         token.email = profile.email;
       }
       token.id = token.sub;
-      /* token
-      {
-  name: 'Thrishank 98',
-  email: 'thrishankkalluru16@gmail.com',
-  picture: 'https://lh3.googleusercontent.com/a/ACg8ocLN8hVLdW3TOvAKgVanA8n92RCbWHBfT5u756JeDCVzI-5I4Q=s96-c',
-  sub: '118179294646479043616',
-  accessToken: 'ya29.a0AXooCgtNdm3kEmwlhj59A7L2Hfb10YzXJoULYFuin802VZz9_oWV6g4uG5oz2rmqG_2fBdoV9jnQfWKxOJkROIe7CcA9zk9bgcOFvhJ-IKE3u9KzaDhhWBx4sQh21R3-Mk4ShaC8vVAOC7Zli5XE11uZIXwjEmhotgaCgYKAVgSARISFQHGX2MidhVIxSRaSzzQgcOw-JQOvg0169',
-  iat: 1717659764,
-  exp: 1720251764,
-  jti: '24aa080f-63a9-45ca-a9a2-1068dce679d3',
-  id: '118179294646479043616'
-}
-      */
-
+     
       return token;
-    },
-    async session({ session, token }: any) {
-      session.user.email = token.email;
-      session.user.account = token.access_token;
-
-      return session;
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
