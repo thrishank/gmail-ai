de62c170692b9ab6816242052421985e1a4f984a
thrishank <thrishankkalluru@gmail.com>
2024-08-09 00:24:40 +0530

    add worldcoin

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,15 +1,11 @@
-/**
- * This is the Signin page which  allows users to log in using their Google account.
- * Users can enter their Gemini AI API key to authenticate.
- * If the API key is provided, it is stored in localStorage.
- * If the user is already authenticated, they are redirected to the homepage.
- * Users can click on the Google login button to initiate the login process.
- */
-
 'use client';
 import { signIn, useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 import { FcGoogle } from 'react-icons/fc';
+
+import { VerificationLevel, IDKitWidget, useIDKit } from '@worldcoin/idkit';
+import type { ISuccessResult } from '@worldcoin/idkit';
+import { verify } from '../actions/verify';
 
 export default function Login() {
   const handleLogin = async () => {
@@ -22,6 +18,36 @@ export default function Login() {
     redirect('/');
   }
 
+  const app_id = process.env.NEXT_PUBLIC_WLD_APP_ID as `app_${string}`;
+  const action = process.env.NEXT_PUBLIC_WLD_ACTION;
+
+  if (!app_id) {
+    throw new Error('app_id is not set in environment variables!');
+  }
+  if (!action) {
+    throw new Error('action is not set in environment variables!');
+  }
+
+  const { setOpen } = useIDKit();
+
+  const onSuccess = (result: ISuccessResult) => {
+    window.alert(
+      'Successfully verified with World ID! Your nullifier hash is: ' +
+        result.nullifier_hash,
+    );
+    // This is where you should perform frontend actions once a user has been verified,
+    // such as redirecting to a new page
+  };
+
+  const handleProof = async (result: ISuccessResult) => {
+    const data = await verify(result);
+    if (data.success) {
+      // store data in db and implement authentication logic(JWT, Cookie)
+    } else {
+      throw new Error(`Verification failed: ${data.detail}`);
+    }
+  };
+
   return (
     <div className="flex h-screen items-center justify-center bg-slate-300">
       <div className="w-96 rounded-md bg-white p-4 text-center">
@@ -31,6 +57,19 @@ export default function Login() {
         >
           <FcGoogle className="mr-2" />
           Login with Google
+        </button>
+        <IDKitWidget
+          action={action}
+          app_id={app_id}
+          onSuccess={onSuccess}
+          handleVerify={handleProof}
+          verification_level={VerificationLevel.Device}
+        />
+        <button
+          className="m-4 rounded-md border border-black"
+          onClick={() => setOpen(true)}
+        >
+          <div className="mx-3 my-1">Verify with World ID</div>
         </button>
       </div>
     </div>
